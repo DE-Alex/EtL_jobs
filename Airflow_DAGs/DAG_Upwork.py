98b26d7bb797a3fd8ad20a83674b6f49370de69f
DE-Alex <139635578+DE-Alex@users.noreply.github.com>
2023-10-02 05:55:35 +0300

    Add Aiflow DAGs folder and Upwork DAG

---
+++ Airflow_DAGs/DAG_Upwork.py
@@ -0,0 +1,30 @@
+﻿from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash_operator \
+      import BashOperator
+from airflow.utils.dates import days_ago
+
+dag = DAG(
+    'Upwork',
+    description='Check and download jobs from www.upwork.com',
+    schedule_interval=timedelta(minutes=10),
+    start_date = days_ago(1),
+    catchup=False,
+)
+
+
+t1 = BashOperator(
+    task_id='check_new_jobs',
+    depends_on_past=False,
+    bash_command='~/upwork_scanner/env/bin/python ~/upwork_scanner/main_1_check.py',
+    dag=dag,
+)
+
+t2 = BashOperator(
+    task_id='download_jobs',
+    depends_on_past=False,
+    bash_command='~/upwork_scanner/env/bin/python ~/upwork_scanner/main_2_download.py',
+    dag=dag,
+)
+
+t1 >> t2
